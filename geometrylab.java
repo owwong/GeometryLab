8ce54c9f3e83fb0c45e6750f708f11fc402e9eb7
owwong <owenwonger@gmail.com>
2015-09-30 19:16:00 +0000

    This is your commit message.

---
+++ geometrylab.java
@@ -0,0 +1,52 @@
+public class GeometryLab {
+
+    public int f(int x, int y) {
+        return x + y;
+    }
+        
+    public int g(int x, int y) {
+        return x - y;
+    }
+    
+    public int h(int x, int y) {
+        return x * y;
+    }
+    
+    public int k(int x, int y) {
+        return x % y;
+    }
+    
+    public int j(int x, int y) {
+        return x / y;
+    }
+    
+    public int m(int x, int y, int z) {
+        return x + y - z;
+        
+         
+         
+         
+        public int triArea(int b, int h) {
+        return 1/2 * b * h;
+    }
+    
+    public int trapArea(int b1, int b2, int h) {
+        return 1/2 * h * (b1 + b2)
+    }
+    
+    public int rectArea(int b, int h) {
+        return b * h;
+    }
+    
+    public int circArea(int r) {
+        return 3.14 * r * r;
+    }
+    
+    public int paralArea(int b, int h) {
+        return b * h;
+    }
+    
+    public int phy
+    }
+}
+    
